ed806ff689902a795d4c92171563ce1ae4124a47
Greice Mara <sgreicemaraj@gmail.com>
2021-04-21 18:37:08 -0300

    pwa init

---
+++ src/containers/Main/components/Panel.jsx
@@ -0,0 +1,70 @@
+import React, { memo } from 'react'
+import RefreshIcon from '../../../assets/images/refresh.svg'
+import { Card, Typography, Button, Select, MenuItem } from '../../../components'
+import COUNTRIES from '../../../commons/constants/countries'
+import { CardPanelContentStyled, ItemStyled } from './style'
+
+const navigatorHasShare = navigator.share
+
+function Panel({ updateAt, onChange, data, country, getCoviddata }) {
+  const { cases, recovered, deaths, todayCases, todayDeaths } = data
+
+  const renderCountries = (country, index) => (
+    <MenuItem key={`country-${index}`} value={country.value}>
+      <ItemStyled>
+        <div>{country.label}</div>
+        <img src={country.flag} alt={`País-${country.label}`} />
+      </ItemStyled>
+    </MenuItem>
+  )
+
+  const textCovid19 = `País: ${country} - ${todayCases} novos casos e ${todayDeaths} mortos nas últimas 24h`
+
+  const copyInfo = () => {
+    navigator.clipboard.writeText(textCovid19)
+  }
+
+  const shareInfo = () => {
+    navigator.share({
+      title: `Dados da Covid-19 - ${country}`,
+      text: textCovid19,
+      url: 'https://covid19dio.netlify.app/'
+    })
+  }
+
+  const renderShareButton = (
+    <div>
+      <Button variant="contained" color="primary" onClick={shareInfo}>
+        Compartilhar
+      </Button>
+    </div>
+  )
+
+  const renderCopyButton = (
+    <div>
+      <Button variant="contained" color="primary" onClick={copyInfo}>
+        Copiar
+      </Button>
+    </div>
+  )
+
+  return (
+    <Card>
+      <CardPanelContentStyled>
+        <div>
+          <Typography variant="h5" component="span" color="primary">Covid-19 ➞ </Typography>
+          <Typography variant="h6" component="span" color="primary">América do Sul<br></br></Typography>
+          <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
+          <div className="pt-2">
+            <Select onChange={onChange} value={country}>
+              {COUNTRIES.map(renderCountries)}
+            </Select>
+          </div>
+        </div>
+        {navigatorHasShare ? renderShareButton : renderCopyButton}
+      </CardPanelContentStyled>
+    </Card>
+  )
+}
+
+export default memo(Panel)
